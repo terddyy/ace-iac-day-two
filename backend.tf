a323046765acf51642136915ea805601ba0a179b
terddyy <terddy03@gmail.com>
2025-08-01 22:46:15 +0800

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
-    # organization = "<terddyorg>"
+    # organization = "<terddy>"
     workspaces {
       name = "ace-iac-day-two"
     }
